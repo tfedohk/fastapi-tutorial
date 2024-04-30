c634b5d291ed8a3520f018995f805b700909d818
kwon dohyung <dohk325@gmail.com>
2024-04-30 13:20:18 +0900

    fix - add new commnet

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from app.core.db.session import ping_db, close_db
 
 
-@asynccontextmanager
+@asynccontextmanager  # 함수에 함수로 감싸는 데코레이터. 지금은 큰 기능은 없더라도, 더 복잡한 기능 구현에 쓰일 것이다.
 async def lifespan(app: FastAPI):
     await ping_db()
 
