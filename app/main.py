bcb25ce700ae879a451c736ee1d6f09fd0716397
kwon dohyung <dohk325@gmail.com>
2024-05-02 12:47:18 +0900

    feat - add api depends

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.lifespan import lifespan
 from app.routers import router
-from app.core.errors.error import BaseAPIException, BaseAuthExeption
+from app.core.errors.error import BaseAPIException, BaseAuthException
 from app.core.errors.handler import api_error_handler, api_auth_error_handler
 from app.core.middlewares.sqlalchemy import SQLAlchemyMiddleware
 
@@ -14,7 +14,7 @@ app = FastAPI(lifespan=lifespan, **config.fastapi_kwargs)
 
 app.include_router(router)
 app.add_exception_handler(BaseAPIException, api_error_handler)
-app.add_exception_handler(BaseAuthExeption, api_auth_error_handler)
+app.add_exception_handler(BaseAuthException, api_auth_error_handler)
 
 app.add_middleware(
     CORSMiddleware,
